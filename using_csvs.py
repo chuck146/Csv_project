1df62e6f561dfdfdabefaf1551b2d1df6e9dab03
Chuck146 <sam98@njit.edu>
2020-03-21 01:55:01 -0400

    Initial commit

---
+++ using_csvs.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('escape_char.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',', escapechar='|')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["name"]} lives at {row["address"]} and joined on {row["date joined"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
